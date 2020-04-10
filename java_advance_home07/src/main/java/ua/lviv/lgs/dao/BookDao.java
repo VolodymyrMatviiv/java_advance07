c75de054fcba1e3b2724b305b3566743d2b668f3
VolodymyrMatviiv <vovamatviiv@gmail.com>
2020-04-10 17:41:35 +0300

    added project to github

---
+++ java_advance_home07/src/main/java/ua/lviv/lgs/dao/BookDao.java
@@ -0,0 +1,8 @@
+package ua.lviv.lgs.dao;
+
+import ua.lviv.lgs.domain.Book;
+import ua.lviv.lgs.shared.AbstractCRUD;
+
+public interface BookDao extends AbstractCRUD<Book> {
+
+}
---
+++ java_advance_home07/src/main/java/ua/lviv/lgs/domain/CustomerRole.java
@@ -0,0 +1,6 @@
+package ua.lviv.lgs.domain;
+
+public enum CustomerRole {
+	ADMINISTRATOR,USER
+
+}
